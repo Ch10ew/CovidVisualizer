70fcbaa8d247637ddd73e596233756e2f1f680ab
Ch10ew <ch10ewthrowaway@gmail.com>
2022-08-01 02:24:20 +0800

    chore: boilerplate code for Dear ImGui & ImGui-SFML

--- README.md
+++ README.md
@@ -8,8 +8,16 @@ Functional programming implementation of a COVID-19 dataset visualizer with Prol
 
 ## Dependencies
 Dependencies will be automatically pulled from github using CMake
-* [SFML 2.5.1](https://www.sfml-dev.org/download/sfml/2.5.1/)
+* [SFML](https://www.sfml-dev.org/download/sfml/2.5.1/)
 * [fast-cpp-csv-parser](https://github.com/ben-strasser/fast-cpp-csv-parser)
+* [Dear ImGui](https://github.com/ocornut/imgui)
+* [ImGui-SFML](https://github.com/eliasdaler/imgui-sfml)
+
+Tested version set:
+* SFML - branch 2.5.x, commit 2f11710abc5aa478503a7ff3f9e654bd2078ebab
+* fast-cpp-scv-parser - branch master, commit 5a417973b4cea674a5e4a3b88a23098a2ab75479
+* Dear ImGui - branch master, commit e99c4fc6688e218a0e5da50f56638aebab45da9b
+* ImGui-SFML - branch master, commit a80d9547832c57a149cda452ed9456def5bb6328
 
 ## CMake
 Example:
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
-#include <SFML/Audio.hpp>
-#include <SFML/Graphics.hpp>
+#include <imgui-SFML.h>
+#include <imgui.h>
+
+#include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/System/Clock.hpp>
+#include <SFML/Window/Event.hpp>
+
 #include <csv.h>
+
 #include <iostream>
 
 int main()
@@ -16,23 +23,40 @@ int main()
         std::cout << "a=" << a << ", b=" << b << ", c=" << c << std::endl;
     }
 
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(640, 480), "ImGui + SFML = <3");
+    window.setFramerateLimit(60);
+    ImGui::SFML::Init(window);
+
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
 
+    sf::Clock deltaClock;
     while (window.isOpen())
     {
         sf::Event event;
         while (window.pollEvent(event))
         {
+            ImGui::SFML::ProcessEvent(window, event);
+
             if (event.type == sf::Event::Closed)
+            {
                 window.close();
+            }
         }
+
+        ImGui::SFML::Update(window, deltaClock.restart());
+
+        ImGui::Begin("Hello, world!");
+        ImGui::Button("Look at this pretty button");
+        ImGui::End();
 
         window.clear();
         window.draw(shape);
+        ImGui::SFML::Render(window);
         window.display();
     }
+
+    ImGui::SFML::Shutdown();
 
     return 0;
 }
